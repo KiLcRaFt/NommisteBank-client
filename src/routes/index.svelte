92854f05c6170de28252d275e8f6d558692f88f5
opilane <killcraft96@gmail.com>
2024-09-18 15:46:15 +0300

    As a user I can register [Delivers #188295901]

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -44,7 +44,7 @@
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Tere tulemast</figcaption>
+	<figcaption>Tere tulemast!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
